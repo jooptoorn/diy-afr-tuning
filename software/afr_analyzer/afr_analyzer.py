3daf19f6590590e59b803a5ce3157fbd65e667e7
Joop <jooptoorn@gmail.com>
2024-05-27 12:29:00 +0200

    add filter for throttle variations

--- software/afr_analyzer/afr_analyzer.py
+++ software/afr_analyzer/afr_analyzer.py
@@ -8,8 +8,9 @@ from scipy.stats import norm
 plt.rcParams['axes.grid'] = True
 
 logFilePath = "C:\\Users\\joopt\\OneDrive\\motor\\rsv mille\\tuning\\logger\\testing\\20240520 first ride no accel pump"
+# logFileFile = "warmup.log"
+# logFileFile = "1st-ride-w-max-rev.log"
 logFileFile = "ride_home.log"
-# logFilePath = "C:\\Users\\joopt\\OneDrive\\motor\\1st-ride-w-max-rev.log"
 
 # fuel table throttle position columns in fuel table of PCIII
 ftTpVals = [0.0, 2.0, 5.0, 10.0, 20.0, 40.0, 60.0, 80.0, 100.0]
@@ -27,6 +28,24 @@ ftTpValLim = 10.0     # example: extract all values around 80% with ftTpScaleLim
 
 # since rpm rows always have fixed distance of either 500rpm or 250rpm, just use fixed value here
 ftRpmValLim = 200.0   # example: extract all values around 5.000rpm with ftRpmValLim=200.0 => range is [4.800 5.200]
+
+# filtering parameters
+# 
+#
+#       Throttle variation.
+#
+# Taking a window of 2*filTpWindowLen + 1 sample, calculate the average Tp value within the window. 
+# Sum the magnitude of the delta of each sample with that average to obtain a measure of the variation 
+# within the window, accepting (an average) maximum variation of filTpMaxSumDelta per sample
+# Consider X samples before and after a sample to detect variations
+filTpWindowLen = 2
+
+# This parameter is the variation PER SAMPLE, during filtering it will be multiplied with window size to
+# obtain a total maximum sum for the entire window
+filTpMaxSumDelta = 3.0
+
+# Throw away filTpFilterLen samples after end of the throttle variation to ignore 'after effects' on AFR of the variation
+filTpFilterLen = 12
 
 # function to extract all values around a certain fuel table cell defined by a combination of tps and rpm value
 def extractLogVals(tps, rpm, ld):
@@ -111,14 +130,107 @@ def printAfrTable(ast):
             rowStrU = rowStrU + afrText + '\t'
             rowStrD = rowStrD + stdText + '\t'
         # print(rowStrU)
-        # print(rowStrD)
+        print(rowStrD)
+
+# print average standard deviation in AFR-statistics table. Used for debugging only
+def printAfrStdAvg(ast):
+    # loop over all elements and add any valid standard deviation (i.e. not NaN) to array
+    stdSum = []
+    for i in range(0,len(ftRpmVals)):
+        for j in range(0,len(ftTpVals)):
+            cell = ast[j][i]
+            std = cell[3][1]
+            if(not(np.isnan(std))):
+                stdSum.append(std)
+    stdAvg = sum(stdSum)/len(stdSum)
+    print("Average standard deviation in AFR values: {:.2f}".format(stdAvg))
 
 # remove logdata where the throttle has just been opened or closed
-# throttle variation causes considerable AFR fluctuations and we do not
-# want to base fuel map improvements on such values. This can only be done for 
+# throttle variations cause considerable AFR fluctuations and we do not
+# want to base fuel map improvements on such values. 
+# This particular filtering can only be done for 
 # the original log where values are chronologically ordered
 def filterThrottleVariation(ld):
+    # check if logfile contains enough entries so we can analyse at least 1 window
+    if(ld.shape[0] < 2*filTpWindowLen + 1):
+        print("Error: not enough log entries for throttle variation filtering")
+        return []
+
+    # copy all logdata first, then start throwing away invalid portions of the log
     fld = ld
+    
+    # total sum-of-deltas in a filter window will be compared against maximum allowable delta
+    # first need to scale the allowable delta-per-sample with window size
+    maxSumDelta = filTpMaxSumDelta * (filTpWindowLen*2 + 1)
+
+    # initialize the vector that will delete samples in fld
+    filterAllIdx = []
+
+    # Now scan for throttle data. For each variation, find the beginning and end
+    # loops over the target samples, skipping first and last samples
+    i = filTpWindowLen
+    while i < ld.shape[0]-filTpWindowLen:
+        # calculate average Tp value around the target
+        avgTp = np.average(ld[i-filTpWindowLen:i+filTpWindowLen+1,1])
+
+        # calculate sum-of-delta around the target sample
+        deltaTpVec = np.abs(ld[i-filTpWindowLen:i+filTpWindowLen+1,1] - avgTp)
+        deltaTpSum = np.sum(deltaTpVec)
+
+
+        # is the sum larger than the allowed maximum?
+        if(deltaTpSum > maxSumDelta):
+            # This is the start of a variation. Find the end using the same arithmetic
+            idxVarStart = i
+
+
+            for j in range(i+1, ld.shape[0]-filTpWindowLen):
+                # calculate average Tp value around the target
+                avgTp = np.average(ld[j-filTpWindowLen:j+filTpWindowLen+1,1])
+
+                # calculate sum-of-delta around the target sample
+                deltaTpVec = np.abs(ld[j-filTpWindowLen:j+filTpWindowLen+1,1] - avgTp)
+                deltaTpSum = np.sum(deltaTpVec)
+
+                # is the sum smaller than the allowed maximum? We found the end of the variation
+                if(deltaTpSum <= maxSumDelta):
+                    break
+            
+            # j is now either at the end of the variation or at the end of the logdata
+            idxVarEnd = j
+            if(idxVarEnd + filTpFilterLen >= ld.shape[0]):
+                # reached end of logdata. Filter out everything up to the last sample
+                idxFilterEnd = ld.shape[0]-1
+            else:
+                # reached the end of the variation. Filter out all data up to X samples after the variation
+                idxFilterEnd = idxVarEnd + filTpFilterLen
+
+            # indexes of logdata to be removed for this variation
+            # filterIdx = [idxVarStart, idxFilterEnd]
+
+            # append to array of all variations
+            for k in range(idxVarStart, idxFilterEnd + 1):
+                filterAllIdx.append(k)
+
+            # set i to new value to prevent multiple detections of same variation
+            # note that we may detect a new variation within the section that will be cut out
+            # as i is set to idxVarEnd rather than idxFilterEnd. This is done on purpose because
+            # any variation will lead to more AFR fluctuations, and we should try to detect ALL variations
+            # including in parts that will be deleted.
+            i = idxVarEnd
+
+        else: # no variation was found
+            i = i+1
+    
+    # at this point all data was scanned. See if there are variations, and delete samples from logdata if so 
+    if(filterAllIdx != []):
+        # remove duplicate deletions
+        filterAllIdx = np.unique(filterAllIdx)
+        fld = np.delete(fld, filterAllIdx, 0)
+    
+    # print("total number of entries deleted: {:d}".format(tot))
+    print("total number of entries deleted: {:d}".format(len(filterAllIdx)))
+
     return fld
 
 #
@@ -164,10 +276,14 @@ if __name__ == "__main__":
     logData = readLogData(logFilePath + "\\" + logFileFile)
     print('Number of valid data samples found:')
     print(logData.shape[0])
+
+    filteredLogData = filterThrottleVariation(logData)
     # eVals = extractLogVals(tps=5.0, rpm=4000.0, ld=logData)
     # stats = calcAfrStat(eVals)
     # print(stats)
-    ast = calcAfrTable(logData)
+    ast = calcAfrTable(filteredLogData)
+    
+    printAfrStdAvg(ast)
     printAfrTable(ast)
     
 
